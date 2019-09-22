2861898b150990bde73ccccc1a3425eb3c0fb729
liushuai <770722922@qq.com>
2019-09-23 02:54:45 +0800

    feature: update log

--- socketpp/log.h
+++ socketpp/log.h
@@ -1,27 +1,38 @@
-//
-// Created by liushuai on 2019/7/10.
-//
-// 方便打印日志 为了输出顺序一致 错误也使用stdout而不用stderr 使用ERROR前缀
-//
+/**
+ * 方便打印日志
+ *
+ * 宏    含义    作用                       颜色
+ * LOG:  LOG    等效于printf并换行         默认色
+ * LOGT: TAG    第一个参数为tag            默认色
+ * LOGI: INFO   带有文件名信息             默认色
+ * LOGE: ERROR  错误信息 会有ERROR:前缀     红色
+ * LOGD: DEBUG  debug模式（NDEBUG未定义时） 黄色
+ * 为了保证输出顺序 错误也使用stdout而不是stderr
+ */
 
 #pragma once
 
+#ifdef __cplusplus
+#include <cstdio>
+#include <cstring>
+#else
 #include <stdio.h>
 #include <string.h>
+#endif
 
 // Truncates the full __FILE__ path, only displaying the basename
 #define __FILENAME__ \
-    (strrchr(__FILE__, '/') ? strrchr(__FILE__, '/') + 1 : __FILE__)
+    (strrchr(__FILE__, '/') ? strrchr(__FILE__, '/') + 1 : strrchr(__FILE__, '\\') ? strrchr(__FILE__, '\\') + 1 : __FILE__)
 
-#define LOGF(fmt, ...)          do{ printf(fmt "\n", ##__VA_ARGS__); } while(0)
+#define LOG(fmt, ...)           do{ printf(fmt "\n", ##__VA_ARGS__); } while(0)
 #define LOGT(tag, fmt, ...)     do{ printf(tag ": " fmt "\n", ##__VA_ARGS__); } while(0)
-#define LOGI(fmt, ...)          do{ fprintf(stdout, "%s: " fmt "\n", __FILENAME__, ##__VA_ARGS__); } while(0)
-#define LOGE(fmt, ...)          do{ perror(""); \
-                                    fprintf(stdout, "ERROR: %s: %s: %d: " fmt "\n", __FILENAME__, __func__, __LINE__, ##__VA_ARGS__); \
+#define LOGI(fmt, ...)          do{ printf("%s: " fmt "\n", __FILENAME__, ##__VA_ARGS__); } while(0)
+#define LOGE(fmt, ...)          do{ printf("\033[31mERROR: %s: %s: %d: " fmt "\033[m\n", \
+                                    __FILENAME__, __func__, __LINE__, ##__VA_ARGS__); \
                                 } while(0)
 
 #ifdef NDEBUG
-    #define LOGD(fmt, ...)  ((void)0)
+#define LOGD(fmt, ...)          ((void)0)
 #else
-    #define LOGD(fmt, ...)  LOGI(fmt, ##__VA_ARGS__)
+#define LOGD(fmt, ...)          do{ printf("\033[33m%s: " fmt "\033[m\n", __FILENAME__, ##__VA_ARGS__); } while(0)
 #endif
